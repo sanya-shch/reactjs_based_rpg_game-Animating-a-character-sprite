5239ae0f60f6a6d8576eac552aba7f6ab4f29d2f
sanya-shch <shchipkov.sanya@ukr.net>
2021-01-02 01:36:13 +0200

    fix;

--- src/App.js
+++ src/App.js
@@ -2,14 +2,10 @@ import React from "react";
 
 import Player from "./components/player";
 
-import './App.css';
-
 function App() {
   return (
     <div className="zone-container">
-      <Player
-        skin="m1"
-      />
+      <Player skin="m1" />
     </div>
   );
 }
